e9563efbae1f6d7a5c4ffb80d16e91d7730d8bdc
Lukas Reimer <lukas.reimer@icloud.com>
2018-03-08 11:20:26 +0100

    added some logic to team related views

--- teams/views.py
+++ teams/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Team, Player
 
 
 def teams(request):
-    return render(request, "teams/teams.html")
+    teams_list = Team.objects.all()
+    return render(request, "teams/teams.html", {"teams_list": teams_list})
 
 
 def team(request, team_id):
-    return render(request, "teams/team.html", {"team_id": team_id})
+    team_data = get_object_or_404(Team, id=team_id)
+    return render(request, "teams/team.html", {"team_data": team_data})
 
 
 def players(request):
